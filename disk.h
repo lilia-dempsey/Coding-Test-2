cd1b2aef3789eea78daba25a0488a57b4e5787ff
Lilia <lcf.dempsey@gmail.com>
2026-02-21 17:00:51 +0000

    created disk.h and used refman to write

---
+++ disk.h
@@ -0,0 +1,17 @@
+#ifndef DISK_H
+#define DISK_H
+
+class Disk {
+    public:
+    Disk(double x, double y, double r)
+    
+    void move(double dx, double dy);
+    
+    double distance(Disk& d);
+
+    double radius;
+    double x;
+    double y;
+};
+
+#endif
