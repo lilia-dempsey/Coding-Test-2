43e666346168c86feef6039250dd153accc5f4d0
Lilia <lcf.dempsey@gmail.com>
2026-02-21 15:33:31 +0000

    Initial commit with .gitignore

---
+++ disk.cpp
@@ -0,0 +1,19 @@
+#include "disk.h"
+#include <cmath>
+
+Disk::Disk(double x, double y, double r){
+    this->x = x;
+    this->y = y;
+    this->radius = r;
+}
+
+void Disk::move(double dx, double dy){
+    this->x += dx;
+    this->y += dy;
+}
+
+double Disk::distance(Disk& other){
+
+    // HINT: COMPLETE THE CALCULATION OF THE DISTANCE BETWEEN this DISK AND THE DISK other
+
+}
